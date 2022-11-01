c11c46eb435e80a629ced4bf873c02b2224e8016
Bogdan <bsshul@gmail.com>
2022-11-01 23:40:25 +0200

    +++

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,9 +1,35 @@
+import styled, { keyframes } from 'styled-components';
 import styles from './Spinner.module.scss'
 
 export const Spinner = () => {
+  let degrees = (Math.random() * (1000 - 240) + 240);
+  let seconds = (Math.random() * (6 - 4) + 4);
+
+  const rotate = keyframes`
+  from {transform: rotate(0deg);}
+  to {transform: rotate(${degrees}deg);}
+`;
+
+  const Container = styled.div`
+  position: absolute;
+  top: 62%;
+  left: 10%;
+  background-color: rgb(255, 253, 242);
+  border-radius: 50%;
+  text-align: center;
+  width: 400px;
+  height: 400px;
+  z-index: 1;
+  overflow: hidden;
+  animation: ${rotate} ${seconds}s ease-in-out forwards;
+`;
+
   return (
-    <div className={styles.spinner}>
-      spinner
-    </div>
+    <Container>
+      <div className={styles.spinner_one}>Пропустити хід</div>
+      <div className={styles.spinner_two}>2</div>
+      <div className={styles.spinner_three}>3</div>
+      <div className={styles.spinner_four}>4</div>
+    </Container>
   )
 }
--- src/screens/Game-screen/Game-screen.jsx
+++ src/screens/Game-screen/Game-screen.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { GuessedWord } from '../../components/GuessedWord'
 import { Keyboard } from '../../components/Keyboard'
@@ -7,17 +8,32 @@ import { Information } from '../../components/Information'
 import { Pause } from '../../components/Pause'
 import { Result } from '../../components/Result'
 import { questions } from '../../data/questions'
+import { Spinner } from '../../components/Spinner'
+import styled from 'styled-components'
 import styles from './Game-screen.module.scss'
-import { Spinner } from '../../components/Spinner/Spinner'
 
 export const GameScreen = () => {
 
   const { firstPlayerScore, secondPlayerScore } = useSelector(store => store.player)
   const { activeCategory, activeQuestionIndex } = useSelector(store => store.game)
 
+  const [spinner, setSpinner] = useState(false)
+
   const answer = questions[activeCategory][activeQuestionIndex].answer.toUpperCase().split('')
   const totalScore = firstPlayerScore + secondPlayerScore
   const gameFinished = totalScore === answer.length
+
+  const Button = styled.button`
+  position: absolute;
+  bottom: 40px;
+  right: 40px;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 8px;
+`;
 
   return (
     <>
@@ -28,8 +44,8 @@ export const GameScreen = () => {
           <Players />
           <Question />
           <GuessedWord />
-          <Spinner />
-          <Keyboard />
+          {spinner ? <Spinner /> : <Keyboard />}
+          <Button onClick={() => { setSpinner(!spinner) }}>Spinner</Button>
         </div>
       }
     </>
